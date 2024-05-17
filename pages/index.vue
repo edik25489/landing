e3a6cfbba19abf5d8e891c17f22e7a6b19517ead
Eduard_yakovlev <edik25mas@yandex.ru>
2024-05-17 11:53:16 +0300

    17:05

--- pages/index.vue
+++ pages/index.vue
@@ -72,11 +72,11 @@ definePageMeta({
   </ClientOnly>
 
 
-  <v-main>
+  <v-main class="mt-4">
     <v-container>
       <v-row>
-        <v-col cols="12" sm="6">
-          <p class="text-green-darken-2 text-h5 text-sm-h4">
+        <v-col cols="12" sm="7" class="d-flex flex-column justify-space-between">
+          <p class="text-green-darken-2 text-h5 text-sm-h3">
             Разработка сайтов и Web-приложений
           </p>
           <v-list>
@@ -86,11 +86,38 @@ definePageMeta({
               variant="flat"
             >
                 <v-icon color="green" icon="mdi-check-circle"></v-icon>
-                ghfg
+              Индивидуальный подход к разработке
+            </v-list-item>
+            <v-list-item
+              color="primary"
+              variant="flat"
+            >
+              <v-icon color="green" icon="mdi-check-circle"></v-icon>
+              Соблюдение дедлайна
+            </v-list-item>
+            <v-list-item
+              color="primary"
+              variant="flat"
+            >
+              <v-icon color="green" icon="mdi-check-circle"></v-icon>
+
             </v-list-item>
           </v-list>
+          <v-btn width="400" elevation="8">
+            <template #prepend>
+              <v-icon color="green" icon="mdi-bell-plus"></v-icon>
+            </template>
+            <template #default>
+              <span class="font-weight-bold text-green-darken-2">оставить заявку</span>
+            </template>
+          </v-btn>
         </v-col>
-        <v-col cols="12" sm="6">2 columns</v-col>
+        <v-col
+          sm="5"
+          class="d-none d-md-flex rounded-ts-circle"
+          style="background: #7DF1E8; height: 350px;"
+        >
+        </v-col>
       </v-row>
     </v-container>
   </v-main>
